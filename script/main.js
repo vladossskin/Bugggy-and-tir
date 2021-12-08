dcdd8ac95b80c4108a51ec35c7d5b10ca7a13f95
vladosssss <vladyslav801@gmail.com>
2021-12-08 12:07:17 +0300

    version1

---
+++ script/main.js
@@ -0,0 +1,79 @@
+// Initialize Swiper
+
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: false,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
+
+    // pagination: {
+    //     el: ".swiper-pagination",
+    // },
+
+
+
+});
+
+
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    slidesOffsetAfter: 3,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: false,
+        depth: 100,
+        modifier: 5,
+        slideShadows: false,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
+
+
+
+});
---
+++ send.php
@@ -0,0 +1,5 @@
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
